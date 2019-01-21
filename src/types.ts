d6e9a7355aca8d4d70f6f1e99da73ea0ceaa679f
Jahed Ahmed <jahed.public@gmail.com>
2019-01-21 02:02:36 +0000

    fix: modifiers parameter is optional

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ type ClassName = string
 type Modifiers = { [key: string]: string | number | boolean | null | undefined }
 type CSSModule = { [key: string]: ClassName }
 type ArrayOrVarArgs<T> = T[] | T[][]
-type BEM = (elementName: ClassName, modifiers: Modifiers) => ClassName
+type BEM = (elementName: ClassName, modifiers?: Modifiers) => ClassName
 
 export {
   ClassName,
